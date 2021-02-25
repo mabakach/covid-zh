e8749da85cc2802abfdd3f4ccbeff78e3dac238b
Marc Baumgartner <marc@mabaka.ch>
2021-02-25 13:00:42 +0100

    Remove unused tests

--- src/test/java/ch/mabaka/covid/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ch.mabaka.covid;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
